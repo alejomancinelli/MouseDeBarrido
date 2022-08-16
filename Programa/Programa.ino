2692cd138b8194bae7a353fa5e87e5714895060f
NahuelErnesto <nahuellogrippo@gmail.com>
2022-08-16 11:00:53 -0300

    Matriz led

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -2,6 +2,7 @@
  //----------------------------------------------------------------Pines
  // USB(0,1) PULSADOR_USUARIO(2) PULSADOR_VELOCIDAD(3) BUZZER(5) PULSADOR_CONTROL(6) LED_VEL(9) LED_CONTROL(10) BT(7,8)
  // MATRIZ LEDS(4,14,15,16,A0,A1,A2,A3)
+
  
 //------------------------------------------------------------------Librerías
 
@@ -16,12 +17,17 @@ void setup() {
 //Entradas salidas
 //USB
 //BT
-//Timers
+noInterrupts();
+TCCR1A = 0;
+TCCR1B = 0;
+TCNT1 = 0;
+OCR1A =31250;
+TCCR1B |= (1 << WGM12); //CTC
+TCCR1B |= (1 << CS12);      //prescaler
+TIMSK1 |= ()1 << OCIE1A);
+interrupts();
 //Valores iniciales
-
 }
-
-
 //------------------------------------------------------------------Loop
 void loop() {
   
@@ -56,6 +62,9 @@ switch (Modo){
 
 
 //------------------------------------------------------------------ISR
+ISR(TIMER1_COMPA_vect){
+  
+  }
 //Bandera temporal
 
 
